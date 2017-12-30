93c5506167556e4c06ff1f109c6c1b8132dc274d
thabiso.shangase <thabiso.shangase@entelect.co.za>
2017-12-30 20:38:43 +0200

    cleaning up configs and scripts and imports

--- SQL/prodo tables.sql
+++ SQL/prodo tables.sql
@@ -1,75 +1,80 @@
 CREATE TABLE profile_status (
-  profileStatusId BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  profilePicturePath VARCHAR(45),
-  pictureName VARCHAR(15),
-  header VARCHAR(200)
+  profile_status_id BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+  profile_picture_path VARCHAR(45),
+  picture_name VARCHAR(15),
+  header VARCHAR(200),
+  PRIMARY KEY(profile_status_id)
 );
 
+CREATE TABLE file_info (
+  file_id BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  file_name VARCHAR (15) NOT NULL,
+  file_nize VARCHAR (7),
+  file_path VARCHAR (45) NOT NULL
+);
 
 CREATE TABLE user_info (
-  userId BIGINT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  userName VARCHAR(15) UNIQUE NOT NULL,
-  firstName VARCHAR(10) NOT NULL,
-  lastName VARCHAR(10) NOT NULL,
-  profileStatusId BIGINT NOT NULL,
-  FOREIGN KEY(profileStatusId)
-  REFERENCES profile_status(profileStatusId)
+  user_id BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(15) UNIQUE NOT NULL,
+  first_name VARCHAR(10) NOT NULL,
+  last_name VARCHAR(10) NOT NULL,
+  profile_status_id BIGINT NOT NULL,
+  PRIMARY KEY(user_id),
+  FOREIGN KEY(profile_status_id)
+  REFERENCES profile_status(profile_status_id)
 );
 
 CREATE TABLE portfolio (
-  portfolioId BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  userId BIGINT NOT NULL,
+  portfolio_id BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  user_id BIGINT NOT NULL,
   heading VARCHAR(200),
-  FOREIGN KEY (userId)
-  REFERENCES user_info(userId)
-);
-
-CREATE TABLE file (
-  fileId BIGINT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  fileName VARCHAR (15) NOT NULL,
-  fileSize VARCHAR (7),
-  filePath VARCHAR (45) NOT NULL,
+  FOREIGN KEY (user_id)
+  REFERENCES user_info(user_id)
 );
 
 CREATE TABLE portfolio_associated_file (
-  portfolioId BIGINT NOT NULL,
-  fileId BIGINT NOT NULL,
-  FOREIGN KEY (portfolioId)
-  REFERENCES portfolio(portfolioId),
-  FOREIGN KEY (fileId)
-  REFERENCES file(fileId)
+  asscociated_file_id BIGINT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+  portfolio_id BIGINT NOT NULL,
+  file_id BIGINT NOT NULL,
+  FOREIGN KEY (portfolio_id)
+  REFERENCES portfolio(portfolio_id),
+  FOREIGN KEY (file_id)
+  REFERENCES file_info(file_id)
 );
 
 CREATE TABLE post (
-  postId BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  postDate DATETIME NOT NULL,
-  userId BIGINT NOT NULL,
-  postMessage VARCHAR(235),
-  FOREIGN KEY (userId)
-  REFERENCES user_info(userId)
+  post_id BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  post_date DATETIME NOT NULL,
+  user_id BIGINT NOT NULL,
+  post_message VARCHAR(235),
+  FOREIGN KEY (user_id)
+  REFERENCES user_info(user_id)
 );
 
 CREATE TABLE post_file (
-  postFileId BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  postId BIGINT NOT NULL,
-  fileId BIGINT NOT NULL,
-  FOREIGN KEY (postId)
-  REFERENCES post(postId),
-  FOREIGN KEY (fileId)
-  REFERENCES file(fileId)
+  post_file_id BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  post_id BIGINT NOT NULL,
+  file_id BIGINT NOT NULL,
+  FOREIGN KEY (post_id)
+  REFERENCES post(post_id),
+  FOREIGN KEY (file_id)
+  REFERENCES file_info(file_id)
 );
 
-CREATE TABLE comment (
-  commentId BIGINT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
-  commentMessage VARCHAR (235),
-  postId BIGINT NOT NULL,
-  FOREIGN KEY (postId)
-  REFERENCES post(postId)
+CREATE TABLE comment_info (
+  comment_id BIGINT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT,
+  comment_message VARCHAR (235),
+  post_id BIGINT NOT NULL,
+  FOREIGN KEY (post_id)
+  REFERENCES post(post_id)
 );
 
 CREATE TABLE comment_file (
-  commentFileId UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  commentId BIGINT NOT NULL,
-  FOREIGN KEY (commentId)
-  REFERENCES comment(commentId)
+  comment_file_id BIGINT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  comment_id BIGINT NOT NULL,
+  file_id BIGINT NOT NULL,
+  FOREIGN KEY (comment_id)
+  REFERENCES comment_info(comment_id),
+  FOREIGN KEY (file_id)
+  REFERENCES file_info(file_id)
 );
