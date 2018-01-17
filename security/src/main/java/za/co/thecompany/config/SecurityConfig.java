547dc364d2b2185c687e03b196e99125ffad905d
thabiso.shangase <thabiso.shangase@entelect.co.za>
2018-01-17 18:48:08 +0200

    adding spring security

--- security/src/main/java/za/co/thecompany/config/SecurityConfig.java
+++ security/src/main/java/za/co/thecompany/config/SecurityConfig.java
@@ -1,24 +1,55 @@
 package za.co.thecompany.config;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import za.co.thecompany.config.user.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+        auth.authenticationProvider(authenticationProvider());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/assets/**").permitAll()
+                .antMatchers("/register", "/", "/catalog", "/product", "/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .usernameParameter("username").passwordParameter("password")
+                .and().csrf()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+        System.out.println("Configured");
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- security/src/main/java/za/co/thecompany/config/user/UserDetailsServiceImpl.java
+++ security/src/main/java/za/co/thecompany/config/user/UserDetailsServiceImpl.java
@@ -1,16 +1,47 @@
 package za.co.thecompany.config.user;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import za.co.thecompany.dao.CommonDao;
+import za.co.thecompany.mapping.UserInfo;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    CommonDao commonDao;
 
-    @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetailsServiceImpl(CommonDao commonDao) {
+        this.commonDao = commonDao;
     }
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserInfo user = commonDao.findUserByUsername(username);
+        UserDetails userDetails = null;
+        if(user != null) {
+            userDetails = buildSpringUser(user);
+        }
+        return userDetails;
+    }
+
+    public UserDetails buildSpringUser(UserInfo userInfo) {
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        String username = userInfo.getUsername();
+        String password = userInfo.getUserPassword();
+
+        authorities.add(new SimpleGrantedAuthority(userInfo.getUserRole()));
+
+        UserDetails currentUser = new User(username, password, authorities);
+        return currentUser;
+    }
+
 }
