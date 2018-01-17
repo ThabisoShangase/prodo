b4c04eb4c65a527be206763ce1fc1e9613aea9d0
thabiso.shangase <thabiso.shangase@entelect.co.za>
2018-01-17 19:20:18 +0200

    clean up

--- Application/src/main/java/application/Application.java
+++ Application/src/main/java/application/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
+                                  HibernateJpaAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
