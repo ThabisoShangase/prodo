93f65d84de6e2fe8bd3502869a445607a8bc551f
thabiso.shangase <thabiso.shangase@entelect.co.za>
2018-01-17 18:46:24 +0200

    adding dao common methods

--- persistence/src/main/java/za/co/thecompany/dao/CommonDao.java
+++ persistence/src/main/java/za/co/thecompany/dao/CommonDao.java
@@ -2,9 +2,19 @@ package za.co.thecompany.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import za.co.thecompany.mapping.UserInfo;
 
 import java.io.Serializable;
 
 public interface CommonDao {
 
+    Serializable save(Serializable entity);
+
+    void delete(Serializable entity);
+
+    void update(Serializable entity);
+
+    Serializable get(Class<Serializable> entityClass, Long id);
+
+    UserInfo findUserByUsername(String username);
 }
--- persistence/src/main/java/za/co/thecompany/dao/impl/CommonDaoImpl.java
+++ persistence/src/main/java/za/co/thecompany/dao/impl/CommonDaoImpl.java
@@ -3,8 +3,10 @@ package za.co.thecompany.dao.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import za.co.thecompany.dao.CommonDao;
+import za.co.thecompany.mapping.UserInfo;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class CommonDaoImpl implements CommonDao {
 
@@ -14,19 +16,32 @@ public class CommonDaoImpl implements CommonDao {
         this.hibernateTemplate = hibernateTemplate;
     }
 
+    @Override
     public Serializable save(Serializable entity) {
         return hibernateTemplate.save(entity);
     }
 
+    @Override
     public void delete(Serializable entity) {
         hibernateTemplate.delete(entity);
     }
 
+    @Override
     public void update(Serializable entity) {
         hibernateTemplate.update(entity);
     }
 
+    @Override
     public Serializable get(Class<Serializable> entityClass, Long id) {
         return hibernateTemplate.get(entityClass, id);
+    }
+
+    @Override
+    public UserInfo findUserByUsername(String username) {
+        List<UserInfo> users = (List<UserInfo>)hibernateTemplate.findByNamedQueryAndNamedParam("FIND_USER_BY_USERNAME", "username", username);
+        if(!users.isEmpty()) {
+            return users.get(0);
+        }
+        return null;
     }
 }
